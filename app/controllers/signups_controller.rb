4a782c399b1b5b1fda0091b9fa4ce21bbcafd038
codecademy <ccuser@codecademy.com>
2016-04-29 14:28:07 -0400

    signup works, thanks page done

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,15 @@
+class SignupsController < ApplicationController
+  def new
+    @signup = Signup.new
+  end
+  def create 
+    @signup = Signup.new(signup_params) 
+    if @signup.save 
+      redirect_to '/thanks' 
+    end 
+	end  
+  private
+  def signup_params
+    params.require(:signup).permit(:firstname,:email)
+  end
+end
